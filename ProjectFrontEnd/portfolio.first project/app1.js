3e4c73368bd99766a44f8d7f0926a1a6f11d74a9
Aysel Mustafayeva <mustafayevaaysel25@gmail.com>
2021-11-01 21:31:56 +0400

    Portfolio

--- ProjectFrontEnd/portfolio.first project/app1.js
+++ ProjectFrontEnd/portfolio.first project/app1.js
@@ -27,85 +27,78 @@ let kv6=document.querySelectorAll('.kvadrat')[5]
 function Accordion(elem){
  box.style.display='none'
  box2.style.display='grid'
+ box4.style.display='block'
+ box5.style.display='block'
+ box6.style.display='block'
 }
-function contact(){
-  box31.style.display='none'
-  box4.innerHTML='+994553819027'
-  box2.style.top='130px'
-}
- function contact2(){
-  box32.style.display='none'
-  box5.innerHTML='ayselmustafayeva25@gmail.com'
-  box33.style.top='65px'
-  
- }
-  
- function contact3(){
-  box33.style.display='none'
-  box6.innerHTML='My telegram'
- }
 
 function project1(){
   kv1.style.display='block'
   pr01.style.display='none'
-  pr0.style.background='none'
+  pr0.style.display='none'
 }
 
 function project2(){
   kv2.style.display='block'
   pr11.style.display='none'
-  pr1.style.background='none'
+  pr1.style.display='none'
 }
 function project3(){
   kv3.style.display='block'
   pr21.style.display='none'
-  pr2.style.background='none'
+  pr2.style.display='none'
 }
 function project4(){
   kv4.style.display='block'
   pr31.style.display='none'
-  pr3.style.background='none'
+  pr3.style.display='none'
 }
 function project5(){
   kv5.style.display='block'
   pr41.style.display='none'
-  pr4.style.background='none'
+  pr4.style.display='none'
 }
 function project6(){
   kv6.style.display='block'
   pr51.style.display='none'
-  pr5.style.background='none'
+  pr5.style.display='none'
 }
 function back1(){
   kv1.style.display='none'
   pr0.style.display='block'
-  pr0.style.backgroundImage = "url('img/quest.png')"
+  pr0.style.backgroundImage = "url('img/qalka3.png')"
   pr01.style.display='block'
 }
 function back2(){
   kv2.style.display='none'
+  pr1.style.display='block'
+  pr1.style.backgroundImage = "url('img/qalka3.png')"
   pr11.style.display='block'
-  pr1.style.backgroundImage="url('img/quest.png')"
+  
 }
 function back3(){
   kv3.style.display='none'
   pr21.style.display='block'
-  pr2.style.backgroundImage="url('img/quest.png')"
+  pr2.style.display='block'
+  pr2.style.backgroundImage = "url('img/qalka3.png')"
 }
 function back4(){
   kv4.style.display='none'
   pr31.style.display='block'
-  pr3.style.backgroundImage="url('img/quest.png')"
+  pr3.style.display='block'
+  pr3.style.backgroundImage = "url('img/qalka3.png')"
 }
 function back5(){
   kv5.style.display='none'
   pr41.style.display='block'
-  pr4.style.backgroundImage="url('img/quest.png')"
+  pr4.style.display='block'
+  pr4.style.backgroundImage = "url('img/qalka3.png')"
 }
 function back6(){
   kv6.style.display='none'
   pr51.style.display='block'
-  pr5.style.backgroundImage="url('img/quest.png')"
+  pr5.style.display='block'
+  pr5.style.backgroundImage = "url('img/qalka3.png')"
 }
 
 
